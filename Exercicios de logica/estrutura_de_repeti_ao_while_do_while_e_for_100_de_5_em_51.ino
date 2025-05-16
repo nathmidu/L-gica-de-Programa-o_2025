c45205b3920b3c54971c2300766ebf59d0f06118
Nathalia Duarte <nathmidu@gmail.com>
2025-05-16 11:25:51 -0300

    exercicios desafios 16/05

---
+++ Exercicios de logica/estrutura_de_repeti_ao_while_do_while_e_for_100_de_5_em_51.ino
@@ -0,0 +1,51 @@
+/*
+While - E uma estrutura de repetiçao que so eecuta a
+repetiçao quando e verdadeira
+*/
+
+//A funçao SETUP() tem como objetivo de iniciar o sistema, serve como
+//uma configuraçao
+void setup()
+{
+  Serial.begin(9600);
+  
+  /*Tipos de dados (Principais)
+   -int(numeros inteiros)
+    -long(numeros inteiros- é um int com maior capacidade de armazenamento)
+    -string(caracteres: alfanumerios e especiais)
+    -float(Números com casas decimais)
+    -bool(true e false)
+    */
+    //Váriavel inicializadora
+    int contadorwhile = 0; //variavel local
+    
+  while(contadorwhile <= 100){
+    Serial.println(contadorwhile);
+    contadorwhile += 5;
+  
+  }
+  
+  /*operadores
+  
+  aritméticos:=,-,*,/,%
+  lógicos: 
+  - || (PIPI - OU)
+  - && (E COMERCIAL - E)
+  -! (PONTO DE EXCLAMAÇAO - NEGAÇAO/INVERTE)
+  
+   relacional:
+   - == (COMPARAÇAO)
+   - >= (MAIOR QUE E IGUAL)
+   - > (MAIOR QUE)
+   - <= (MENOR QUE E IGUAL)
+   - != (DIFERENTE DE)
+   - < (MENOR QUE)
+   */
+  
+}
+
+//A funçao LOOP() tem como objetivo de repitir varias vezes o programa
+void loop()
+{
+   //sem instruçoes
+}
